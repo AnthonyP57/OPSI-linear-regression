fd25bba3e357e1b46e1c303c34fddb2d858d19db
AnthonyP57 <antonipawlowicz123@gmail.com>
2025-03-23 21:00:08 +0100

    minor fixes

--- LinReg.py
+++ LinReg.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch.nn as nn
 import torch
+from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 import warnings
@@ -8,28 +9,26 @@ import matplotlib.pyplot as plt
 from modules import timeit
 import pandas as pd
 
-warnings.filterwarnings("ignore")
-
 class NumpyLinReg:
     def __init__(self, in_features, bias=False, lr=0.1):
-        self.weights = np.random.random(in_features) # ~ (f)
-        self.bias = np.random.random(1) if bias else np.zeros(1) # ~ (1)
+        self.weights = np.random.randn(in_features) # ~ (f)
+        self.bias = np.random.randn(1) if bias else np.zeros(1) # ~ (1)
         self.calc_bias = bias
         self.lr = lr
 
     def forward(self, x):
         # x ~ (n, f)
-        return self.weights @ x.T + self.bias
+        return x @ self.weights + self.bias
     
     def update_weights(self, x, y):
         weight_sum = np.dot(self.weights, x)
         for i in range(len(self.weights)):
             derivative = - x[i] * (y - weight_sum - self.bias) # SE = 1/2 * (y - y')^2
-            self.weights[i] -= self.lr*derivative
+            self.weights[i] -= self.lr*derivative.item()
 
     def update_bias(self, x, y):
-        derivative = - (y - self.weights @ x.T - self.bias) # SE = 1/2 * (y - y')^2
-        self.bias -= self.lr*derivative
+        derivative = - (y - x @ self.weights - self.bias) # SE = 1/2 * (y - y')^2
+        self.bias -= self.lr*derivative.item()
     
     @timeit
     def train(self, x, y, epochs=100):
@@ -64,8 +63,8 @@ class NumpyLinRegCloseForm:
             x = np.hstack((x, np.ones((x.shape[0], 1))))
         try:
             self.weights = np.linalg.inv(x.T @ x) @ x.T @ y
-        except Exception as e:
-            print(e)
+        except:
+            warnings.warn("matrix inverse failed, using pseudo inverse instead", Warning)
             self.weights = np.linalg.pinv(x.T @ x) @ x.T @ y
     
     def forward(self, x):
@@ -75,7 +74,6 @@ class NumpyLinRegCloseForm:
     
     def validate(self, x, y):
         y_pred = self.forward(x)
-        y = y.reshape(-1)
         return np.mean((y - y_pred)**2), r2_score(y, y_pred)
 
 
@@ -98,9 +96,12 @@ class TorchLinReg(nn.Module):
     def train(self, x, y, epochs=100):
         if self.device:
             x, y = x.to(self.device), y.to(self.device)
+        # data = TensorDataset(x, y)
+        # data_loader = DataLoader(data, batch_size=1, shuffle=True)
         loss_fn = nn.MSELoss()
         optimizer = torch.optim.SGD(self.parameters(), lr=self.lr)
         for _ in range(epochs):
+            # for x, y in data_loader:
             y_pred = self.forward(x)
             loss = loss_fn(y_pred, y)
             optimizer.zero_grad()
@@ -117,9 +118,9 @@ if __name__ == "__main__":
 
     # REVERSE COMMENTS HERE TO SWITCH BETWEEN LR AND MLR (no visualization)
     x = np.arange(0, 1000, 1, dtype=np.float32).reshape(-1, 1)/1000
-    # x = np.hstack((x, x*1.5))
-    # y = np.sum(x, axis=1).reshape(-1, 1) + 0.1
-    y = x*2 + 1
+    x = np.hstack((x, x*1.5))
+    y = np.sum(x, axis=1).reshape(-1, 1) + 0.1
+    # y = x*2 + 1
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.05, random_state=42)
 
@@ -128,6 +129,7 @@ if __name__ == "__main__":
     model = NumpyLinReg(x.shape[1], bias=True, lr=1e-1)
     model.train(x_train, y_train)
     nlt_metrics = model.validate(x_test, y_test)
+    nlt_metrics_train = model.validate(x_train, y_train)
     nlt_weights = np.hstack((model.weights, model.bias))
 
     # plt.plot((min(x_test), max(x_test)), (min(y_test), max(y_test)), linestyle='--', color='red', linewidth=1, label='True Values')
@@ -143,6 +145,7 @@ if __name__ == "__main__":
     closed = NumpyLinRegCloseForm(bias=True)
     closed.fit(x_train, y_train)
     nltc_metrics = closed.validate(x_test, y_test)
+    nltc_metrics_train = closed.validate(x_train, y_train)
     clr_weights = closed.weights.reshape(-1) if closed.bias else np.concat((closed.weights.reshape(-1), np.zeros(1)))
     
     # plt.plot((min(x_test), max(x_test)), (min(y_test), max(y_test)), linestyle='--', color='red', linewidth=1, label='True Values')
@@ -160,6 +163,7 @@ if __name__ == "__main__":
     model = TorchLinReg(x.shape[1], bias=True, lr=1e-1, device=None)
     model.train(x_train, y_train)
     tlr_metrics = model.validate(x_test, y_test)
+    tlr_metrics_train = model.validate(x_train, y_train)
     tlt_weights = np.hstack((model.l.weight.detach().numpy().reshape(-1), model.l.bias.detach().numpy())) if model.bias else np.concat((model.l.weight.detach().numpy().reshape(-1), np.zeros(1)))
     
     # plt.plot((min(x_test), max(x_test)), (min(y_test), max(y_test)), linestyle='--', color='red', linewidth=1, label='True Values')
@@ -172,8 +176,9 @@ if __name__ == "__main__":
     # plt.close()
 
     data = np.vstack((nlt_metrics, nltc_metrics, tlr_metrics))
+    data = np.hstack((data, np.vstack((nlt_metrics_train, nltc_metrics_train, tlr_metrics_train))))
     data = np.hstack((data, np.vstack((nlt_weights, clr_weights, tlt_weights)))).round(3)
-    metrics = pd.DataFrame(columns=['MSE', 'R2']+[f'weight_{i}' for i in range(x.shape[1])]+['bias'], index=['NumpyLinReg', 'NumpyLinRegCloseForm', 'TorchLinReg'], data=data)
+    metrics = pd.DataFrame(columns=['MSE test', 'R2 test', 'MSE train', 'R2 train']+[f'weight_{i}' for i in range(x.shape[1])]+['bias'], index=['NumpyLinReg', 'NumpyLinRegCloseForm', 'TorchLinReg'], data=data)
     print(metrics.to_string())
 
 
